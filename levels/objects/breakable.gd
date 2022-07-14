a8f703b55461a059954460a45a21e40fc41bb4de
Ross <ross@eml.cc>
2022-07-13 19:47:33 -0700

    Breakable variety

--- levels/objects/breakable.gd
+++ levels/objects/breakable.gd
@@ -2,27 +2,31 @@ extends StaticBody2D
 
 const rune = preload('res://pickups/runes/rune.tscn')
 var rng = RandomNumberGenerator.new()
+var sprite = "001"
 
 func _ready():
-	pass
+	rng.randomize()
+	var sprite_num = rng.randi_range(1, 6)
+	sprite = str(sprite_num).pad_zeros(3)
+	$Sprite.set_texture(load("res://levels/objects/Rock_" + sprite + ".png"))
 
 
 func break():
-	$Crumbling.visible = true
-	$Crumbling.play()
-	$Sprite.visible = false
+	$Sprite.set_texture(load("res://levels/objects/Rockbroken_" + sprite + ".png"))
+	$AnimationPlayer.play("fade")
 
 
-func _on_Crumbling_animation_finished():
-	rng.randomize()
-	var has_loot = rng.randi_range(0, 1)
-	if has_loot:
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "fade":
 		rng.randomize()
-		# Runes, Scrolls, ???
-		var loot_type = rng.randi_range(0, 0)
-		if loot_type == 0:
-			# Spawn a rune
-			var rune_instance = rune.instance()
-			rune_instance.global_position = global_position
-			get_parent().add_child(rune_instance)
-	queue_free()
+		var has_loot = rng.randi_range(0, 1)
+		if has_loot:
+			rng.randomize()
+			# Runes, Scrolls, ???
+			var loot_type = rng.randi_range(0, 0)
+			if loot_type == 0:
+				# Spawn a rune
+				var rune_instance = rune.instance()
+				rune_instance.global_position = global_position
+				get_parent().add_child(rune_instance)
+		queue_free()
