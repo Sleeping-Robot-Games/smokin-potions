5dd06e766e399d443f7f0bcc9ffe8c1f0e646352
Ross <ross@eml.cc>
2022-07-13 17:49:17 -0700

    Match timer

--- levels/game/game.gd
+++ levels/game/game.gd
@@ -3,6 +3,9 @@ extends Node
 onready var wizard = preload('res://players/wizard/wizard.tscn')
 onready var bot = preload("res://players/bots/bot.tscn")
 
+onready var match_time = get_node("HUD/MatchTime")
+var seconds = 120
+
 func _ready():
 	g.connect("elements_changed", self, "handle_elements_changed")
 	for player in g.players_in_current_game:
@@ -26,3 +29,18 @@ func handle_elements_changed(elements):
 	else:
 		$HUD/Element1.texture = null
 		$HUD/Element2.texture = null
+
+
+func _on_MatchTimer_timeout():
+	if seconds > 0:
+		seconds -= 1
+		if seconds <= 0:
+			match_time.bbcode_text = '[center][color=red]OVERTIME[/color][/center]'
+		else:
+			match_time.bbcode_text = '[center]' + format_time() + '[/center]'
+	else:
+		match_time.bbcode_text = '[center][color=red]OVERTIME[/color][/center]'
+
+
+func format_time():
+	return str(abs(ceil(seconds/60))).pad_zeros(2) + ":" + str("%01d" % abs(ceil(seconds % 60))).pad_zeros(2)
