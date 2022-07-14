fadc48727ba27f63739c93922a0f03343594e541
Bryan <bryan87reed@gmail.com>
2022-07-14 15:30:58 -0700

    Connects portraits to player number

--- levels/wizard_sprites.gd
+++ levels/wizard_sprites.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 export (int) var frame: int = 3
+export (String) var number: String = "1"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for sprite in get_children():
 		sprite.frame = frame
+	g.load_player(self, number)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
