80b8ed075c51e1a4c2d8af44f0126d190a92891d
Ross <ross@eml.cc>
2022-07-15 15:09:07 -0700

    Despawn runes

--- pickups/runes/rune.gd
+++ pickups/runes/rune.gd
@@ -24,3 +24,12 @@ func set_type(type):
 
 func cleanup():
 	queue_free();
+
+
+func _on_DespawnTimer_timeout():
+	$AnimationPlayer.play("despawn")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "despawn":
+		queue_free()
