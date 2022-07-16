de1d4ebd5169306144dfc89d0e2f69088bc53934
Bryan <bryan87reed@gmail.com>
2022-07-16 12:31:22 -0700

    start game with enter and space

--- menus/start/start.gd
+++ menus/start/start.gd
@@ -9,14 +9,14 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if not g.new_game:
-		visible = false
-		get_parent().get_node("Select").visible = true
+		_on_Button_button_up()
 	$Sprite/AnimationPlayer.play("jiggle")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_released("ui_accept"):
+		_on_Button_button_up()
 
 
 func _on_Button_button_up():
