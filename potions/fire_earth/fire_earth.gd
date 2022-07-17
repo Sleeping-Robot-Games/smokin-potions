d4f7f1f27e4e6c6331c27daba84cfa446850d575
Ross <ross@eml.cc>
2022-07-16 19:23:13 -0700

    metal af lava

--- potions/fire_earth/fire_earth.gd
+++ potions/fire_earth/fire_earth.gd
@@ -1,8 +1,8 @@
 extends 'res://potions/potion.gd'
 
 const lava = preload('res://potions/fire_earth/lava/lava.tscn')
-const size = 3
-const cell = 12
+#const size = 3
+#const cell = 12
 
 func _ready():
 	activate()
@@ -14,19 +14,24 @@ func trigger_effect():
 	$ExplosionArea/Explode.visible = true
 	$ExplosionArea/Explode.play()
 	
-	for x in range(size * -1, size):
-		for y in range(size * -1, size):
-			if (x == size or x == size * -1) and (y == size or y == size * -1):
-				continue
-			if (x == size - 1 or x == (size * -1) + 1) and (y == size - 1 or y == (size * -1) + 1):
-				continue
-			var lava_instance = lava.instance()
-			lava_instance.global_position = Vector2(
-				global_position.x + (x*cell),
-				global_position.y + (y*cell)
-			)
-			lava_instance.last_wiz = last_wiz
-			get_parent().add_child(lava_instance)
+	var lava_instance = lava.instance()
+	lava_instance.global_position = global_position
+	lava_instance.scale = Vector2(2, 2)
+	get_parent().add_child(lava_instance)
+	
+#	for x in range(size * -1, size):
+#		for y in range(size * -1, size):
+#			if (x == size or x == size * -1) and (y == size or y == size * -1):
+#				continue
+#			if (x == size - 1 or x == (size * -1) + 1) and (y == size - 1 or y == (size * -1) + 1):
+#				continue
+#			var lava_instance = lava.instance()
+#			lava_instance.global_position = Vector2(
+#				global_position.x + (x*cell),
+#				global_position.y + (y*cell)
+#			)
+#			lava_instance.last_wiz = last_wiz
+#			get_parent().add_child(lava_instance)
 
 func _on_Explode_animation_finished():
 	queue_free()
--- potions/fire_earth/lava/lava.gd
+++ potions/fire_earth/lava/lava.gd
@@ -3,21 +3,46 @@ extends Node2D
 export (int) var speed = 100
 
 var last_wiz
+var nearby_players = []
+var timers = []
 
 
 func _ready():
-	$Area2D.connect('area_shape_entered', self, '_on_area_shape_entered')
+#	$Area2D.connect('area_shape_entered', self, '_on_area_shape_entered')
 	$Area2D.connect('body_entered', self, '_on_body_entered')
+	$Area2D.connect('body_exited', self, '_on_body_exited')
 
 
-func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
-	if area and weakref(area).get_ref() and ('Breakable' in area.get_parent().name or 'Durable' in area.get_parent().name or 'Boundaries' in area.get_parent().name or 'NoLavaOrIce' in area.get_parent().name):
-		queue_free()
+#func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+#	if area and weakref(area).get_ref() and ('Breakable' in area.get_parent().name or 'Durable' in area.get_parent().name or 'Boundaries' in area.get_parent().name or 'NoLavaOrIce' in area.get_parent().name):
+#		queue_free()
 
 
 func _on_body_entered(body):
 	if body and weakref(body).get_ref() and 'Wizard' in body.name or 'Bot' in body.name:
+		nearby_players.append(body)
 		body.take_dmg(1, self)
+		var timer = Timer.new()
+		timer.wait_time = 1.5
+		timer.one_shot = true
+		timer.autostart = true
+		timer.connect('timeout', self, '_on_timer_timeout', [body])
+		timers.append({"timer": timer, "player": body})
+		add_child(timer)
+
+
+func _on_timer_timeout(body):
+	if body and weakref(body).get_ref() and nearby_players.has(body):
+		body.take_dmg(1, self)
+
+
+func _on_body_exited(body):
+	if body and weakref(body).get_ref() and 'Wizard' in body.name or 'Bot' in body.name:
+		nearby_players.erase(body)
+		for t in timers:
+			if t["player"] == body:
+				t["timer"].queue_free()
+				timers.erase(t)
 
 
 func _on_SelfDestructTimer_timeout():
