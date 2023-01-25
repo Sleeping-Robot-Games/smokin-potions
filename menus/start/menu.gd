176598014607a49b970013f17098288f32d21a2a
Bryan <bryan87reed@gmail.com>
2023-01-25 10:02:29 -0700

    Adds SRG splash and removes GWJ

--- menus/start/menu.gd
+++ menus/start/menu.gd
@@ -4,21 +4,23 @@ var title_scene = preload("res://menus/start/Title.tscn")
 var selection_scene = preload("res://menus/player_selection/selection.tscn")
 var map_scene = preload("res://menus/map_selection/map_selection.tscn")
 
+var title = null
+
 func _ready():
 	if g.new_game:
-		add_child(title_scene.instance())
-		$Splashscreen.visible = true
+		title = title_scene.instance()
+		add_child(title)
+		title.visible = false
+		$SrgSplash.visible = true
+		$SrgSplash.get_node("AnimationPlayer").play("SRG")
+		$SrgSplash.get_node("AnimationPlayer").connect("animation_finished", self, 'on_Animation_finished')
 		var music_player = $AudioStreamPlayer
 		music_player.stream = load('res://sfx/title_screen.mp3')
 		music_player.play()
-		$Timer.start()
 	else:
-		$Splashscreen.visible = false
+		$SrgSplash.visible = false
 		add_child(selection_scene.instance())
 
-	
-func _on_Timer_timeout():
-	$Splashscreen.visible = false
 
 func switch_screen(screen_name, current):
 	if screen_name == 'title':
@@ -29,3 +31,8 @@ func switch_screen(screen_name, current):
 		add_child(map_scene.instance())
 	current.queue_free()
 
+func on_Animation_finished(anim_name):
+	title.visible = true
+	$SrgSplash.visible = false
+	print(anim_name)
+	print('animation done')
